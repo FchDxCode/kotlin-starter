9e18b5ae43ba08d193542d95509e648a12651f9a
Fachru <fachru2006@gmail.com>
2023-01-25 13:49:48 +0700

    project intellij idea

---
+++ src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,17 @@
+import java.awt.Color
+import java.time.chrono.JapaneseEra.valueOf
+import java.time.chrono.JapaneseEra.values
+
+
+fun main(){
+    val color: Color = Color.valueOf("RED")
+    println("Color is $color")
+    println("Color value is ${color.value.toString(16)}")
+}
+
+enum class Color(val value: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+
+}
